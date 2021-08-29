07d4eeb81f19cb7e53580524952fc7c2af5b086d
Joel Burton <joel@joelburton.com>
2021-08-28 18:57:45 -0700

    Add homepage.

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,7 +1,24 @@
-function Homepage(quizzes) {
+import { Link } from "react-router-dom";
+import Markdown from "react-remarkable";
+
+function Homepage({quizzes}) {
   return (
-    <h1>Welcome</h1>
-  )
+    <div className="Homepage">
+      <h1>Welcome</h1>
+      <p className="lead">
+        Welcome to SQL Petting Zoo, the world's leading site for combining
+        live SQL lessons with adorable animal emoji. This site is in beta, but
+        you are welcome to explore it.
+      </p>
+
+      { Object.entries(quizzes).map(([id, quiz]) => (
+        <div className="mt-5" key={id}>
+          <h2><Link className="text-decoration-none" to={`/${id}/`}>{ quiz.title }</Link></h2>
+          <Markdown children={quiz.description} />
+        </div>
+      )) }
+    </div>
+  );
 }
 
 export default Homepage;
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,7 +8,7 @@ import axios from "axios";
  * @returns {Promise<{result, isCorrect}>}
  */
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api";
 
 async function submitSql(quizId, questionNum, sql) {
   const response = await fetch(
